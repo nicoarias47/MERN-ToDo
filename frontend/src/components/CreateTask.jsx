16da8bad6a43ce11a21a99c584faa4e849c8689a
nicoarias47 <ariasn1995@gmail.com>
2023-01-05 00:20:26 -0300

    show tasks

--- frontend/src/components/CreateTask.jsx
+++ frontend/src/components/CreateTask.jsx
@@ -1,33 +1,67 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
+const initialForm = {
+  title: "",
+  description: "",
+  deadline: "",
+};
+
 const CreateSubTask = () => {
+  const [form, setForm] = useState(initialForm);
   const { idList } = useParams();
+
+  const handleChange = ({ target: { value, name } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // --TODO:--
+    // funcion para crear tarea
+  };
 
   return (
     <section className="p-5">
       <div className="pt-4 flex flex-row justify-between">
-        <h1>Tasks</h1>
+        <h1>Create task</h1>
         <Link to={`/${idList}`}>🔙</Link>
       </div>
       <div>
-        <form className="pt-5 flex flex-col gap-3 rounded-md">
+        <form
+          className="pt-5 flex flex-col gap-3 rounded-md"
+          onSubmit={handleSubmit}
+        >
           <div className="flex flex-col">
-            <label htmlFor="title">title</label>
-            <input type="text" name="title" required className="rounded-sm" />
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              name="title"
+              required
+              className="rounded-sm"
+              onChange={handleChange}
+            />
           </div>
           <div className="flex flex-col">
-            <label htmlFor="description">description</label>
+            <label htmlFor="description">Description</label>
             <textarea
               type="text"
               name="description"
-              className="rounded-sm"
+              rows="5"
+              className="rounded-sm p-1 resize-none"
+              onChange={handleChange}
             ></textarea>
           </div>
           <div className="flex flex-col">
             <label htmlFor="date">Deadline</label>
-            <input type="date" name="date" className="rounded-sm" />
+            <input
+              type="date"
+              name="date"
+              className="rounded-sm"
+              onChange={handleChange}
+            />
           </div>
+          <input type="submit" value="send" />
         </form>
       </div>
     </section>
--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const TaskItem = () => {
-  return <div>TaskItem</div>;
+const TaskItem = ({ data }) => {
+  const { title, description, deadline, complete } = data;
+  return (
+    <div className="py-2">
+      <h2>{title}</h2>
+      <span>{description}</span>
+      <span>{deadline}</span>
+    </div>
+  );
 };
 
 export default TaskItem;
